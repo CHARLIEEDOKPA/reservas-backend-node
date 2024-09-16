816557ae66734c6ed6a2291c5e58d670b4352bd8
CHARLIEEDOKPA <jnr.31gcharlieedokpa@gmail.com>
2024-09-16 11:32:49 +0200

    He mudado las constantes del archivo conf.js a archivo .env por temas de seguridad

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -2,15 +2,14 @@ const {Sequelize} = require("sequelize")
 const nodeMailer = require("nodemailer")
 
 
-const HOST = `localhost`
-const DATABASE_NAME = "reservations"
-const DATABASE_USER = "root"
-const DATABASE_PASSWORD = "12345678"
-const DIALECT = "mysql"
-const APPLICATION_PASSWORD = "gcaz erdl ebmv nsuw"
-const USER = "jnr.31gcharlieedokpa@gmail.com"
-require("dotenv").config()
-
+require("dotenv").config();
+const HOST = process.env.HOST;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DIALECT = process.env.DIALECT;
+const APPLICATION_PASSWORD = process.env.APPLICATION_PASSWORD;
+const USER = process.env.USER;
 
 
 const sequelize = new Sequelize(DATABASE_NAME,DATABASE_USER,DATABASE_PASSWORD, {
